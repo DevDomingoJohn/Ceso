245d6f1f3f4d210d59c79f3440f424537a779142
DomingoJohnlery <domingojohnled@gmail.com>
2024-08-16 09:52:56 +0800

    9th Commit: Navigation Drawer & Navigation Drawer Items

--- app/src/main/java/com/domindev/ceso/presentation/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/domindev/ceso/presentation/ui/screens/HomeScreen.kt
@@ -3,6 +3,7 @@ package com.domindev.ceso.presentation.ui.screens
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
@@ -17,24 +18,41 @@ import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.Card
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -49,6 +67,12 @@ import com.domindev.ceso.presentation.ui.theme.bodyFontFamily
 import com.domindev.ceso.presentation.ui.theme.displayFontFamily
 import com.domindev.ceso.R
 import com.domindev.ceso.presentation.ui.viewmodels.DataViewModel
+import kotlinx.coroutines.launch
+
+data class NavigationItem(
+    val title: String,
+    val icon: ImageVector,
+)
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -61,77 +85,139 @@ fun HomeScreen(
     val notes by viewModel.notes.collectAsState()
     val searchText by viewModel.searchText.collectAsState()
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
-    Scaffold(
-        floatingActionButton = {
-            FloatingActionButton(onClick = {
-                navigateTo(NoteScreen)
-                onEvent(Events.ToggleFocus)
-            }) {
-                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Note")
-            }
-        },
-        topBar = {
-            if (state.isSearching) {
-                SearchBar(state = state, searchQuery = searchText, searchResult = notes, onEvent = onEvent) {
-                    navigateTo(NoteScreen)
-                }
-            } else {
-                MyCustomTopBar(
-                    title = "Search Notes",
-                    navigationIcon = {
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
-                        }
-                    },
-                    actions = {
-                        if (!state.columnView) {
-                            IconButton(onClick = { onEvent(Events.ToggleView) }) {
-                                Icon(painter = painterResource(id = R.drawable.baseline_grid_view_24), contentDescription = "Account")
-                            }
-                        } else {
-                            IconButton(onClick = { onEvent(Events.ToggleView) }) {
-                                Icon(painter = painterResource(id = R.drawable.outline_view_agenda_24), contentDescription = "Account")
-                            }
-                        }
 
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Account")
-                        }
-                    },
-                    scrollBehavior = scrollBehavior,
-                    modifier = Modifier
-                        .padding(start = 16.dp, end = 16.dp, top = 10.dp, bottom = 10.dp)
-                        .shadow(elevation = 3.dp, shape = RectangleShape)
-                        .clickable { onEvent(Events.SetSearch(true)) }
-                )
-            }
+    val items = listOf(
+        NavigationItem(
+            title = "Home",
+            icon = Icons.Outlined.Home
+        ),
+        NavigationItem(
+            title = "Trash",
+            icon = Icons.Outlined.Delete
+        ),
+        NavigationItem(
+            title = "Settings",
+            icon = Icons.Outlined.Settings
+        ),
+        NavigationItem(
+            title = "Help & Feedback",
+            icon = Icons.Outlined.Info
+        )
+    )
 
-        },
-        modifier = if (!state.isSearching) {
-            Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
-        } else {
-            Modifier
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+        val coroutineScope = rememberCoroutineScope()
+        var selectedItemIndex by rememberSaveable {
+            mutableIntStateOf(0)
         }
-
-    ) { padding ->
-        if (!state.columnView) {
-            LazyVerticalStaggeredGrid(
-                contentPadding = padding,
-                columns = StaggeredGridCells.Fixed(2)
-            ) {
-                items(state.notes) { note ->
-                    NoteItem(note = note, onEvent = onEvent) {
-                        navigateTo(NoteScreen)
+        ModalNavigationDrawer(
+            drawerContent = {
+                ModalDrawerSheet {
+                    Text(
+                        text = "Ceso",
+                        fontFamily = displayFontFamily,
+                        fontSize = 32.sp,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    items.forEachIndexed { index, item ->
+                        NavigationDrawerItem(
+                            label = { Text(text = item.title) },
+                            icon = { Icon(imageVector = item.icon, contentDescription = item.title)},
+                            selected = index == selectedItemIndex,
+                            onClick = {
+                                selectedItemIndex = index
+                                coroutineScope.launch {
+                                    drawerState.close()
+                                }
+                            },
+                            modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                        )
                     }
                 }
-            }
-        } else {
-            LazyColumn(
-                contentPadding = padding
-            ) {
-                items(state.notes) { note ->
-                    NoteItem(note = note, onEvent = onEvent) {
+            },
+            drawerState = drawerState
+        ) {
+            Scaffold(
+                floatingActionButton = {
+                    FloatingActionButton(onClick = {
                         navigateTo(NoteScreen)
+                        onEvent(Events.ToggleFocus)
+                    }) {
+                        Icon(imageVector = Icons.Default.Add, contentDescription = "Add Note")
+                    }
+                },
+                topBar = {
+                    if (state.isSearching) {
+                        SearchBar(state = state, searchQuery = searchText, searchResult = notes, onEvent = onEvent) {
+                            navigateTo(NoteScreen)
+                        }
+                    } else {
+                        MyCustomTopBar(
+                            title = "Search Notes",
+                            navigationIcon = {
+                                IconButton(onClick = {
+                                    coroutineScope.launch {
+                                        drawerState.open()
+                                    }
+                                }) {
+                                    Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
+                                }
+                            },
+                            actions = {
+                                if (!state.columnView) {
+                                    IconButton(onClick = { onEvent(Events.ToggleView) }) {
+                                        Icon(painter = painterResource(id = R.drawable.baseline_grid_view_24), contentDescription = "Account")
+                                    }
+                                } else {
+                                    IconButton(onClick = { onEvent(Events.ToggleView) }) {
+                                        Icon(painter = painterResource(id = R.drawable.outline_view_agenda_24), contentDescription = "Account")
+                                    }
+                                }
+
+                                IconButton(onClick = { /*TODO*/ }) {
+                                    Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Account")
+                                }
+                            },
+                            scrollBehavior = scrollBehavior,
+                            modifier = Modifier
+                                .padding(start = 16.dp, end = 16.dp, top = 10.dp, bottom = 10.dp)
+                                .shadow(elevation = 3.dp, shape = RectangleShape)
+                                .clickable { onEvent(Events.SetSearch(true)) }
+                        )
+                    }
+
+                },
+                modifier = if (!state.isSearching) {
+                    Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
+                } else {
+                    Modifier
+                }
+
+            ) { padding ->
+                if (!state.columnView) {
+                    LazyVerticalStaggeredGrid(
+                        contentPadding = padding,
+                        columns = StaggeredGridCells.Fixed(2)
+                    ) {
+                        items(state.notes) { note ->
+                            NoteItem(note = note, onEvent = onEvent) {
+                                navigateTo(NoteScreen)
+                            }
+                        }
+                    }
+                } else {
+                    LazyColumn(
+                        contentPadding = padding
+                    ) {
+                        items(state.notes) { note ->
+                            NoteItem(note = note, onEvent = onEvent) {
+                                navigateTo(NoteScreen)
+                            }
+                        }
                     }
                 }
             }
@@ -255,3 +341,4 @@ fun SearchBar(
         }
     }
 }
+
--- app/src/main/java/com/domindev/ceso/presentation/ui/screens/NoteScreen.kt
+++ app/src/main/java/com/domindev/ceso/presentation/ui/screens/NoteScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -71,8 +72,15 @@ fun NoteScreen(
                         }
                     },
                     actions = {
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(painter = painterResource(id = R.drawable.outline_push_pin_24), contentDescription = "pin")
+                        IconButton(onClick = {
+                            /*TODO*/
+                        }) {
+                            Icon(painter = painterResource(id = R.drawable.outline_push_pin_24), contentDescription = "Pin Icon")
+                        }
+                        IconButton(onClick = {
+                            /*TODO*/
+                        }) {
+                            Icon(imageVector = Icons.Default.Share, contentDescription = "Share Icon")
                         }
                         IconButton(onClick = {
                             navigateBack()
